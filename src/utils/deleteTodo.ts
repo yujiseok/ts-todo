2d5a15db6ba8d2fb3f4d5de0a4c630176656c46b
yujiseok <koesijuy@gmail.com>
2022-12-01 19:32:24 +0900

    feat: deleteTodo
    axios error handle 해야함

--- src/utils/deleteTodo.ts
+++ src/utils/deleteTodo.ts
@@ -1,7 +1,39 @@
 import { request } from "../api/request";
+import { toastifyOpen } from "./toast";
 
 const todos = await request("todos", "get");
+const deleteBtns = document.querySelectorAll(".delete-btn");
+const deleteAllbtn = document.querySelector(".deleteAll-btn");
 
-// todos.forEach((todo) => {
-//   request(`todos/${todo.id}`, "delete");
-// });
+deleteBtns.forEach((deleteBtn) => {
+  deleteBtn.addEventListener("click", delteTodo);
+});
+
+deleteAllbtn.addEventListener("click", deleteAll);
+
+// delete Individual
+function delteTodo(e) {
+  const todoItem = e.target.parentNode.parentNode as HTMLLIElement;
+  const id = todoItem.dataset.id;
+  todoItem.classList.add("delete");
+
+  todoItem.addEventListener("transitionend", async () => {
+    todoItem.remove();
+    await request(`todos/${id}`, "delete");
+  });
+  toastifyOpen("할 일이 삭제됐어요 👻", "#ff5252", "#fff");
+}
+
+function deleteAll() {
+  const todoItems = document.querySelectorAll(".todo-item");
+
+  todoItems.forEach((todoItem: HTMLLIElement) => {
+    todoItem.classList.add("delete");
+    const id = todoItem.dataset.id;
+    todoItem.addEventListener("transitionend", async () => {
+      todoItem.remove();
+      await request(`todos/${id}`, "delete");
+    });
+  });
+  toastifyOpen("할 일이 모두 삭제됐어요 💥", "#ff5252", "#fff");
+}
